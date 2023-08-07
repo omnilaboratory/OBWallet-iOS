5575bd26dd1d1c0d7ebab3aa9221ba0b92b921cf
iamgamelover <lunarx.cheng@gmail.com>
2023-08-07 11:45:44 +0800

    Added square button component

---
+++ lib/component/square_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SquareButton extends StatelessWidget {
+  final String icon;
+  final String text;
+  final VoidCallback onPressed;
+
+  const SquareButton({super.key, required this.icon, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: SizedBox(
+        width: 90,
+        height: 67,
+        child: Center(
+          child: Column(
+            children: [
+              Image(image: AssetImage(icon)),
+              const SizedBox(height: 5),
+              Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
