03ecd0bdfe4c36751481f8f84fade5abccc8b671
John <john4swallet@gmail.com>
2023-09-05 21:17:15 +0800

    createTextFormField padding left

--- lib/component/common.dart
+++ lib/component/common.dart
@@ -5,8 +5,9 @@ final OutlineInputBorder outlineInputBorder = OutlineInputBorder(
   borderRadius: BorderRadius.circular(8),
 );
 
-TextFormField createTextFormField(TextEditingController controller, String hintText,
-    Icon? icon, bool obscureText,{int? maxLength}) {
+TextFormField createTextFormField(TextEditingController controller,
+    String hintText, Icon? icon, bool obscureText,
+    {int? maxLength}) {
   return TextFormField(
     controller: controller,
     maxLines: 1,
@@ -14,14 +15,15 @@ TextFormField createTextFormField(TextEditingController controller, String hintT
     decoration: InputDecoration(
       hintText: hintText,
       prefixIcon: icon,
-      hintStyle: const TextStyle(fontSize: 13,fontWeight: FontWeight.w400),
+      hintStyle: const TextStyle(fontSize: 13, fontWeight: FontWeight.w400),
       border: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       enabledBorder: outlineInputBorder,
       disabledBorder: outlineInputBorder,
       focusedErrorBorder: outlineInputBorder,
       errorBorder: outlineInputBorder,
-      contentPadding: const EdgeInsets.only(right: 4,top: 1,bottom: 1),
+      contentPadding: EdgeInsets.only(
+          left: (icon == null ? 10 : 0), right: 4, top: 1, bottom: 1),
     ),
     obscureText: obscureText,
     validator: (v) {
