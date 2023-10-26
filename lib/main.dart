40d956346846df6225569ad23a9cbf982b553fa0
iamgamelover <lunarx.cheng@gmail.com>
2023-10-26 14:39:33 +0900

    Keep the app is showing at portrait up and down

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:awallet/logins/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   debugPaintSizeEnabled = false;
   runApp(const MyApp());
 }
