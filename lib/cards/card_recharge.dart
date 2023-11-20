a245e7d9c3a87f826d655a8d1fabb60b54d5d55a
john <john4swallet@gmail.com>
2023-11-20 14:01:42 +0800

    alert(value.msg, context, () {});

--- lib/cards/card_recharge.dart
+++ lib/cards/card_recharge.dart
@@ -361,6 +361,7 @@ class _CardRechargeState extends State<CardRecharge> {
         }
       } else {
         log(value.msg);
+        alert(value.msg, context, () {});
         if (widget.type == EnumChargeType.withdraw) {
           showToast(Tips.failWithdraw.value);
         } else if (widget.type == EnumChargeType.deposit) {
