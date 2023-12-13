a99b0ba6f15b13160b195c9a1e7e177d6ff21d05
john <john4swallet@gmail.com>
2023-12-13 14:52:35 +0800

    invitationCode

--- lib/profile/my_invitation_code.dart
+++ lib/profile/my_invitation_code.dart
@@ -48,31 +48,42 @@ class _MyInvitationCodeState extends State<MyInvitationCode> {
                       Center(
                         child: Padding(
                           padding: const EdgeInsets.only(top: 28, bottom: 10),
-                          child: createDialogTitle(S.of(context).profile_home_InvitationCode),
+                          child: createDialogTitle(
+                              S.of(context).profile_home_InvitationCode),
                         ),
                       ),
                       const SizedBox(
                         height: 20,
                       ),
-                      Center(
-                          child: GestureDetector(
+                      GestureDetector(
                         onTap: () {
                           Clipboard.setData(
                               ClipboardData(text: invitationCode!));
                           showToast(S.of(context).tips_codeIsOnClipboard);
                         },
-                        child: Text(invitationCode!,
-                            style: const TextStyle(
-                              color: Colors.blue,
-                              fontSize: 40,
-                            )),
-                      )),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(invitationCode!,
+                                style: const TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 40,
+                                )),
+                            const SizedBox(width: 10),
+                            Image.asset(
+                              'asset/images/icon_copy_green.png',
+                              width: 40,
+                            )
+                          ],
+                        ),
+                      ),
                       const SizedBox(
-                        height: 40,
+                        height: 30,
                       ),
                       BottomButton(
                         icon: 'asset/images/icon_confirm_green.png',
                         text: S.of(context).common_Ok.toUpperCase(),
+                        fontSize: 20,
                         onPressed: () {
                           Navigator.of(context).pop();
                         },
