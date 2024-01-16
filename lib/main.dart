6ca67e27d0465059f31c088fdc0d3482196fa09a
john <john4swallet@gmail.com>
2024-01-16 16:44:38 +0800

    CWallet

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,7 @@ class _MyAppState extends State<MyApp> {
         }
         return const Locale('en', 'US');
       },
-      title: 'AWallet',
+      title: 'CWallet',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
