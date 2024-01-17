1d7a85338a7bb9f1521c42fcb1bd3f8d51289ddd
john <john4swallet@gmail.com>
2024-01-17 10:45:51 +0800

    mainnet

--- lib/tools/global_params.dart
+++ lib/tools/global_params.dart
@@ -9,7 +9,7 @@ int pageSize = 10;
 class GlobalParams {
   static EventBus eventBus = EventBus();
 
-  static EnumNetworkType currNetwork = EnumNetworkType.goerli;
+  static EnumNetworkType currNetwork = EnumNetworkType.mainnet;
 
   static int grpcTimeout = 30;
   static String currLangName = "en";
