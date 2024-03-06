a187322a9b1a61f3b7c91cca2b223f14170a278a
john <john4swallet@gmail.com>
2024-03-06 16:32:13 +0800

    AgentCardItem

--- lib/component/agent_card_item.dart
+++ lib/component/agent_card_item.dart
@@ -1,3 +1,4 @@
+import 'package:awallet/cards/physical_card_active.dart';
 import 'package:awallet/protos/gen-dart/user/card.pb.dart';
 import 'package:flutter/material.dart';
 
@@ -13,28 +14,39 @@ class AgentCardItem extends StatefulWidget {
 class _AgentCardItemState extends State<AgentCardItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightGreenAccent,
-      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
-      margin: const EdgeInsets.only(bottom: 6),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(widget.cardInfo.userName),
-              const Spacer(),
-              Text(widget.cardInfo.userEmail),
-            ],
-          ),
-          const SizedBox(height: 4),
-          Row(
-            children: [
-              Text(widget.cardInfo.cardNo),
-              const Spacer(),
-              Text(widget.cardInfo.pcardStatus == 0 ? "Active" : "Inactive"),
-            ],
-          ),
-        ],
+    return InkWell(
+      onTap: () {
+        if (widget.cardInfo.pcardStatus == 0) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return PhysicalCardActive(cardNo: widget.cardInfo.cardNo);
+              });
+        }
+      },
+      child: Container(
+        color: Colors.lightGreenAccent,
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+        margin: const EdgeInsets.only(bottom: 6),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(widget.cardInfo.userName),
+                const Spacer(),
+                Text(widget.cardInfo.userEmail),
+              ],
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text(widget.cardInfo.cardNo),
+                const Spacer(),
+                Text(widget.cardInfo.pcardStatus == 0 ? "Inactive" : "Active"),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
